1e35d32ccdb0b4b30a6ce63a0f3c07a38beeffb4
acretinat <103209471+acretinat@users.noreply.github.com>
2023-03-27 09:55:18 +0200

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
-59 8 * * 1-5 ~/clear.sh
-*/5 9-17 * * 1-5 ~/scrap.sh
-1-30/5 17 * * 1-5 ~/scrap.sh
-00 20 * * 1-5 ~/stats.sh
+59 6 * * 1-5 ~/clear.sh
+*/5 7-15 * * 1-5 ~/scrap.sh
+1-30/5 15 * * 1-5 ~/scrap.sh
+00 18 * * 1-5 ~/stats.sh
